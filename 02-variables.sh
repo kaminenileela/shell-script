be02163a6c7dfb9ad4bd39fc5baa4083bb6f19e2
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-26 20:17:08 -0400

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,1 +1,6 @@
-This is again sample script
+#!/bin/bash
+
+echo "Siva:: Hello Leela, How are you?"
+echo "Leela:: Hello Siva, I am fine.How are you?"
+echo "Siva:: I am fine too. How is your work?"
+echo "Leela:: Not bad. I am thinking to upgrade to DevOps"
