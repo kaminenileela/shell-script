dc5f46a0c047bd34da2ad71e4a8b5d0b433ef2a2
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-26 20:22:49 -0400

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Siva
+PERSON2=Leela
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine.How are you?"
+echo "$PERSON1:: I am fine too. How is your work?"
+echo "$PERSON2:: Not bad. I am thinking to upgrade to DevOps"
