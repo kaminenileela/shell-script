ebdb276ee3569f8fd531de106b33ebaeec119715
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-29 18:19:20 -0400

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R=\e[31m
-G=\e[32m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]
 then
