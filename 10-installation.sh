0f399d7f7601bc1e178523791447b627b535a199
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-29 13:23:01 -0400

    installation

---
+++ 10-installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$((id -u))
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run script with super user access"
+    exit 2
+else
+    echo "You are super user"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of MYSQL...FAILURE"
+    exit 1
+
+else
+    echo "Installation of MYSQL...SUCCESS"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT...FAILURE"
+    exit 1
+
+else
+    echo "Installation of GIT...SUCCESS"
+fi
+
