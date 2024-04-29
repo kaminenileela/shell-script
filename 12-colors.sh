adb5fe45f1f6285b84b421147374368b419da5e2
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-29 18:16:58 -0400

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run script with sudo access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+VALIDATE(){
+
+if [ $1 -eq 0 ]
+then
+    echo -e "$2...$R SUCCESS $N"
+else
+    echo -e "$2....$R FAILURE $N"
+fi
+
+}
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing SQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
+
+dnf install dockerr -y &>>$LOGFILE
+VALIDATE $? "Installing with wrong name"
