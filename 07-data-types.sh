9b9d0a5cac6c362b55d75f020a42c9ebe8fe5b98
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-28 20:22:24 -0400

    data types

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -7,8 +7,10 @@ SUM=$(($NO1+$NO2))
 
 echo "Total of $NO1 and $NO2 is $SUM"
 
-read "Enter no1:" a
-read "Enter no2:" b
+echo "Enter no1:"
+read a
+echo "Enter no2:"
+read b
 
 c= `expr $a-$b`
 echo "Subtraction of $a and $b is $c" 
