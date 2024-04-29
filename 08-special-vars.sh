c8d1bf5788c4bc9eb9caad9c7b3035c04cb6739d
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-28 20:59:02 -0400

    special characters

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,5 +8,6 @@ echo "current working directory $PWD"
 echo "Which user is running the script $USER"
 echo "hostname: $HOSTNAME"
 echo "process id of current shell script: $$"
+sleep 60
 sleep 60 &
 echo "process id of last background command: $!"
