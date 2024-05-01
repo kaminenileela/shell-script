fb669af2cddb99b9f0caf2cd3b23c8634489b1fb
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-30 20:42:44 -0400

    calling other script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,7 @@ COURSE="Devops from current script"
 echo "Before calling  variable from current script: $COURSE"
 echo "process id of current script : $$"
 
-./16-other-script.sh
+#./16-other-script.sh
+source ./16-other-script.sh
 
 echo "After calling  variable from other script: $COURSE"
