f0e0118c02f40278fdfc78a71a963a1ff5374401
kaminenileela <kaminenileela1992@email.com>
2024-05-04 19:37:40 -0400

    disk-usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-echo "$DISK_USAGE"
+THRESHOLD=6
+MESSAGE=""
+
+while IFS = read -r line
+do
+    USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    FOLDER=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+
+    MESSAGE+="$FOLDER usage is $USAGE, exceeds usage limit $THRESHOLD"
+
+done
+while <<< $DISK_USAGE
+
+echo "message: $MESSAGE"
