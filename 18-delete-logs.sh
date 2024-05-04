347f9182a2171392ab75d3cff7144348a2373e76
Leela Kamineni <kaminenileela1992@gmail.com>
2024-05-03 20:29:35 -0400

    delete old logs

---
+++ 18-delete-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app_logs
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo "Source directory exists"
+
+else
+    echo "Please make sure $SOURCE_DIRECTORY  exist"
+fi
+
+FILES=$(find SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+echo "old logs dated 14 days back: $FILES"
